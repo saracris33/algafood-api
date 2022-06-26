2353d52754daa47f6f1bdf3f030e42d70d6fbf2e
Sara <saracristina33@gmail.com>
2022-06-26 19:38:30 -0300

    Implementa subrecurso de grupos por usuario

--- src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.exception.UsuarioNaoEncontradoException;
+import com.algaworks.algafood.domain.model.Grupo;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.UsuarioRepository;
 
@@ -17,6 +18,9 @@ public class CadastroUsuarioService {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private CadastroGrupoService cadastroGrupo;
 	
 
 	@Transactional
@@ -40,6 +44,22 @@ public class CadastroUsuarioService {
 		}
 
 		usuario.setSenha(novaSenha);
+	}
+	
+	@Transactional
+	public void desassociarGrupo(Long usuarioId, Long grupoId) {
+	    Usuario usuario = buscarOuFalhar(usuarioId);
+	    Grupo grupo = cadastroGrupo.buscarOuFalhar(grupoId);
+	    
+	    usuario.removerGrupo(grupo);
+	}
+
+	@Transactional
+	public void associarGrupo(Long usuarioId, Long grupoId) {
+	    Usuario usuario = buscarOuFalhar(usuarioId);
+	    Grupo grupo = cadastroGrupo.buscarOuFalhar(grupoId);
+	    
+	    usuario.adicionarGrupo(grupo);
 	}
 
 	public Usuario buscarOuFalhar(Long usuarioId) {
