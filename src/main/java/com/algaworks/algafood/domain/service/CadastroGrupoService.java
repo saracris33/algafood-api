b8f9194711fc21dd20a36b1fba6935554dfb2a6c
Sara <saracristina33@gmail.com>
2022-06-26 19:29:25 -0300

    Implementa subrecuros de busca, associa e desassocia permissoes de um grupo

--- src/main/java/com/algaworks/algafood/domain/service/CadastroGrupoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroGrupoService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.GrupoNaoEncontradoException;
 import com.algaworks.algafood.domain.model.Grupo;
+import com.algaworks.algafood.domain.model.Permissao;
 import com.algaworks.algafood.domain.repository.GrupoRepository;
 
 @Service
@@ -19,6 +20,9 @@ public class CadastroGrupoService {
 
 	@Autowired
 	private GrupoRepository grupoRepository;
+
+	@Autowired
+	private CadastroPermissaoService cadastroPermissao;
 
 	@Transactional
 	public Grupo salvar(Grupo grupo) {
@@ -37,6 +41,22 @@ public class CadastroGrupoService {
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(String.format(MSG_GRUPO_EM_USO, grupoId));
 		}
+	}
+
+	@Transactional
+	public void associarPermissao(Long grupoId, Long permissaoId) {
+		Grupo grupo = buscarOuFalhar(grupoId);
+		Permissao permissao = cadastroPermissao.buscarOuFalhar(permissaoId);
+
+		grupo.adicionarPermissao(permissao);
+	}
+
+	@Transactional
+	public void desassociarPermissao(Long grupoId, Long permissaoId) {
+		Grupo grupo = buscarOuFalhar(grupoId);
+		Permissao permissao = cadastroPermissao.buscarOuFalhar(permissaoId);
+
+		grupo.removerPermissao(permissao);
 	}
 
 	public Grupo buscarOuFalhar(Long grupoId) {
