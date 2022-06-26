cbdcc8372119af8814204ade359b6508ec973ac4
Sara <saracristina33@gmail.com>
2022-06-26 19:05:54 -0300

    Implementa end poit produto de um restaurante e adiciona endpoint aberto e fechado no restaurante

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -50,6 +50,8 @@ public class Restaurante {
 	
 	private Boolean ativo = Boolean.TRUE;
 	
+	private Boolean aberto = Boolean.FALSE;
+	
 	@CreationTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
 	private OffsetDateTime dataCadastro;
@@ -74,6 +76,14 @@ public class Restaurante {
 	public void inativar() {
 		setAtivo(false);
 	}
+	
+	public void abrir() {
+	    setAberto(true);
+	}
+
+	public void fechar() {
+	    setAberto(false);
+	}  
 	
 	public boolean  adicionarFormaPagamento(FormaPagamento formaPagamento) {
 		return this.formasPagamento.add(formaPagamento);
--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -56,6 +56,20 @@ public class CadastroRestauranteService {
 	}
 	
 	@Transactional
+	public void abrir(Long restauranteId) {
+	    Restaurante restauranteAtual = buscarOuFalhar(restauranteId);
+	    
+	    restauranteAtual.abrir();
+	}
+
+	@Transactional
+	public void fechar(Long restauranteId) {
+	    Restaurante restauranteAtual = buscarOuFalhar(restauranteId);
+	    
+	    restauranteAtual.fechar();
+	} 
+	
+	@Transactional
 	public void associarFormaPagamento(Long restauranteId, Long formaPagamentoId) {
 		Restaurante restaurante = this.buscarOuFalhar(restauranteId);
 		FormaPagamento formaPagamentoExistente = cadastroFormaPagamento.buscarOuFalhar(formaPagamentoId);
