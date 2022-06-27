c98f6adc63eb4ad7eccc20844d27841e6a3dc6f3
Sara <saracristina33@gmail.com>
2022-06-26 21:28:04 -0300

    Aula 12.21 - Implementa adicionar pedido

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.algaworks.algafood.api.model.EnderecoModel;
+import com.algaworks.algafood.api.model.input.ItemPedidoInput;
 import com.algaworks.algafood.domain.model.Endereco;
+import com.algaworks.algafood.domain.model.ItemPedido;
 
 @Configuration
 public class ModelMapperConfig {
@@ -16,9 +18,13 @@ public class ModelMapperConfig {
 		
 		// MAP PARA REFERENCIA ESTRUTURA COMPLEXA
 		var enderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+		
 		enderecoModelTypeMap.<String>addMapping(
 				 enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
 				 (enderecoModelDest,valor) -> enderecoModelDest.getCidade().setEstado(valor));
+		
+		modelMapper.createTypeMap(ItemPedidoInput.class, ItemPedido.class)
+	    .addMappings(mapper -> mapper.skip(ItemPedido::setId)); 
 		
 		return modelMapper;
 	}
